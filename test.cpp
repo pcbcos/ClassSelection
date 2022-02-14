fdc715c3f0802097730c2d32235a24d8df918ba0
pcbcos <oxygen10266@gmail.com>
2022-02-15 02:56:33 +0800

    第一次提交,hash表刚刚完成

--- test.cpp
+++ test.cpp
@@ -26,7 +26,7 @@ auto main(int argc, char *args[]) -> int {
         std::cout << student_list[i].name << ((student_list[i].ID ^ 31415927) % MAX_STUDENT_NUM) << std::endl;
     }
     std::cout<< get_index_by_ID(19211606, student_list, index_of_student_hashID)<<std::endl;
-    std::cout<<student_list[1485].ID<<std::endl;;
+    std::cout<<student_list[1485].ID<<std::endl;;;
 
 
     //getchar();
