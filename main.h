0765277e69871458a4f3a19434996901b627c1b6
cosine <2682582785@qq.com>
2022-02-26 14:09:19 +0800

    update

--- main.h
+++ main.h
@@ -4,20 +4,29 @@
 
 #ifndef CLASSSELECTION_MAIN_H
 #define CLASSSELECTION_MAIN_H
+
 #include <newt.h>
 #include <cstdio>
 #include <cstdlib>
-#include <csignal>
 #include <cstring>
 #include "DataBase.h"
+#include "unistd.h"
+
 void login();
+
 void student_querry();
+
 void student_addclass();
+
 void student_delclass();
 
 
 void admin_lookover();
+
 void admin_querry();
+
 void admin_modify();
+
 void admin_addclass();
+
 #endif //CLASSSELECTION_MAIN_H
